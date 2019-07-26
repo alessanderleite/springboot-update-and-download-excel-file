f8062399272141f79820430ab7a67a9fcd55e1ac
alessanderleite <alessander.logistica@gmail.com>
2019-07-25 21:30:49 -0300

    initial commit

---
+++ src/main/java/br/com/alessanderleite/app/SpringBootUploadAndDownloadExcelFileApplication.java
@@ -0,0 +1,13 @@
+package br.com.alessanderleite.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUploadAndDownloadExcelFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUploadAndDownloadExcelFileApplication.class, args);
+	}
+
+}
