214842099268d71b49061519690c9a1d37d3dfb6
alessanderleite <alessander.logistica@gmail.com>
2019-07-25 23:39:37 -0300

    create the controller class named DownloadFileController.java

---
+++ src/main/java/br/com/alessanderleite/app/controller/DownloadFileController.java
@@ -0,0 +1,33 @@
+package br.com.alessanderleite.app.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.InputStreamSource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import br.com.alessanderleite.app.service.CustomerService;
+
+@Controller
+public class DownloadFileController {
+	
+	@Autowired
+	CustomerService customerService;
+	
+	/*
+	 * Download Files 
+	 */
+	@GetMapping("/file")
+	public ResponseEntity<InputStreamSource> downloadFile() {
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Content-Dispositon", "attachment; filename=customers.xlsx");
+		
+		return ResponseEntity
+				.ok()
+				.headers(headers)
+				.body(new InputStreamResource(customerService.loadFile()));	
+	}
+}
